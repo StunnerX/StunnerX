abb1e380ed4eef5cc2a2b0b044cbf50289f2abbd
Mayank Gupta <Sankygupta1@gmail.com>
2022-08-21 20:15:04 +0530

    Added Sum of Numbers

--- Multiply.py
+++ Multiply.py
@@ -3,3 +3,5 @@ b=2
 print("Multiply of Numbers is: " + str(a*b))
 # This is Mayank/Sum version & 
 # i am checking whether its is accessible by anyone or not
+
+print("Sum of Numbers is: ", str(a+b))
